0df6cbc5d71611899af83a66003521fc2c5511f9
rikusasa <rikuto.sasaki.chiba@gmail.com>
2023-09-09 16:38:54 +0900

    2回目

--- match/views.py
+++ match/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"]="太郎"
+        return ctxt
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["num_survices"]="123456789"
+        ctxt["skills"] = ["Python", "C++", "Java", "Go"]
+        return ctxt
